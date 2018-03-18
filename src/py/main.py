86c9f1ee9ea3f528fd39b6a872363c9cc49ca6e4
Chad Bohannan <chad.bohannan@gmail.com>
2018-03-18 15:46:19 -0600

    Python packet parsing is working.

---
+++ src/py/main.py
@@ -0,0 +1,19 @@
+from packet import Packet, readINT32U, writeINT32U
+from parser import Parser
+
+# import pdb; pdb.set_trace()
+
+def packet_callback(packet):
+    print 'packet parsed with %d data bytes: %s' % (packet.dataSize, "".join(map(chr, packet.data)))
+
+def main():
+    # TODO parse packets from the input file
+    fname = "packetstream.input"
+    parser = Parser(packet_callback)
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(15), ''):
+            parser.readBytes(bytearray(chunk))
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/py/parser.py
@@ -0,0 +1,146 @@
+# Parser class can compose and decompose packets for frame-link transit
+from packet import Packet, crc, CFHamDecode, readINT16U, writeINT16U, readINT32U, writeINT32U
+
+class Parser:
+
+    # Packet parsing state enumeration
+    STATE_FINDSTART = 0
+    STATE_GET_CF    = 1
+    STATE_GETHEADER = 2
+    STATE_GETDATA   = 3
+    STATE_GETCRC    = 4
+
+    # LinkState value enumerations (TODO support source routing)
+    LINK_CONNECT   = 0x01
+    LINK_CONNECTED = 0x03
+    LINK_PING      = 0x05
+    LINK_PONG      = 0x07
+    LINK_ACKRESEND = 0x09
+    LINK_NOACK     = 0x0B
+    LINK_CLOSE     = 0x0D
+
+    # requires a callback function that will be called when a packet is parsed
+    # from incremental data
+    def __init__(self, packet_callback):
+        self.packet_callback = packet_callback
+        self.clear()
+
+    def clear(self):
+        self.state = Parser.STATE_FINDSTART
+        self.delimCount = 0
+        self.controlFlags = 0x0000
+        self.headerIndex = 0
+        self.headerLength = 0
+        self.dataIndex = 0
+        self.dataLength = 0
+        self.crcIndex = 0
+        self.crcLength = 0
+
+    def acceptPacket(self):
+        packet = Packet(self.frameBuffer)
+
+        # deliver to application code
+        if (self.packet_callback):
+            self.packet_callback(packet)
+        else:
+            return -3 # TODO enumerate errors
+
+        self.clear()
+
+        return None
+
+    # consumes data incrementally and emits calls to packet_callback when full
+    # packets are parsed from the incoming stream
+    def readBytes(self, buffer):
+        for msg in buffer:
+
+            # check for escape char (occurs mid-frame)
+            if (msg == Packet.FRAME_ESCAPE):
+                if (self.delimCount >= (Packet.FRAME_LEADER_LENGTH-1)):
+                    self.delimCount = 0 # reset FRAME_LEADER detection
+                    continue # drop the char from the stream
+            elif(msg == Packet.FRAME_LEADER):
+                self.delimCount += 1
+                if (self.delimCount >= Packet.FRAME_LEADER_LENGTH):
+                    self.delimCount = 0
+                    self.headerIndex = 0
+                    self.frameBuffer = []
+                    self.state = Parser.STATE_GET_CF
+                    continue
+            else: # not a framing byte; reset delim count
+                self.delimCount = 0
+
+
+            # use current char in following state
+            if (self.state == Parser.STATE_FINDSTART):
+                # Do Nothing, dump char
+                continue # end STATE_FINDSTART
+
+            if (self.state == Parser.STATE_GET_CF):
+                # TODO isoloate into readControlByte(msg)
+                if(self.headerIndex > Packet.MAX_HEADER_SIZE):
+                    this.state = Parser.STATE_FINDSTART
+                else:
+                    self.frameBuffer.append(msg)
+                    self.headerIndex += 1
+                    if (self.headerIndex == 2):
+                        self.controlFlags = readINT16U(self.frameBuffer)
+                        # self.controlFlags = CFHamDecode(cf)
+                        self.headerLength = Packet.headerLength(self.controlFlags)
+                        self.state = Parser.STATE_GETHEADER
+
+                continue # end STATE_GET_CF
+
+            if (self.state == Parser.STATE_GETHEADER):
+                # TODO isoloate into readHeaderByte(msg)
+                if(self.headerIndex >= Packet.MAX_HEADER_SIZE):
+                    self.state = Parser.STATE_FINDSTART
+                else:
+                    self.frameBuffer.append(msg)
+                    self.headerIndex += 1
+                    if(self.headerIndex >= self.headerLength):
+                        if (self.controlFlags & Packet.CF_DATA):
+                            self.dataIndex = 0
+                            dataOffset = Packet.headerFieldOffset(self.controlFlags, Packet.CF_DATA)
+                            dataBytes = self.frameBuffer[dataOffset:dataOffset+2]
+                            self.dataLength = readINT16U(dataBytes)
+                            self.state = Parser.STATE_GETDATA
+                        elif (self.controlFlags & Packet.CF_CRC):
+                            self.crcIndex = 0
+                            self.dataLength = 0
+                            self.crcLength = Packet.CRC_FIELD_SIZE
+                            self.state = Parser.STATE_GETCRC
+                        else:
+                            self.acceptPacket()
+                continue # end STATE_GETHEADER
+
+            if (self.state == Parser.STATE_GETDATA):
+                # TODO isoloate into readDataByte(msg)
+                self.frameBuffer.append(msg)
+                self.dataIndex += 1
+                if(self.dataIndex >= self.dataLength):
+
+                    if (self.controlFlags & Packet.CF_CRC):
+                        self.crcIndex = 0
+                        self.crcLength = Packet.CRC_FIELD_SIZE
+                        self.state = Parser.STATE_GETCRC
+                    else:
+                        self.acceptPacket()
+                continue # end STATE_GETDATA
+
+            if (self.state == Parser.STATE_GETCRC):
+                # TODO isoloate into readCRCByte(msg)
+                self.frameBuffer.append(msg)
+                self.crcIndex += 1
+                if(self.crcIndex >= self.crcLength):
+                    sz = len(self.frameBuffer)
+                    subframeBytes = self.frameBuffer[0:sz-Packet.CRC_FIELD_SIZE]
+                    computedCRC = crc(subframeBytes)
+                    crcBytes = self.frameBuffer[sz-Packet.CRC_FIELD_SIZE:sz]
+                    expectedCRC = readINT32U(crcBytes)
+                    if (computedCRC != expectedCRC):
+                        # TODO error reporting
+                        self.state = Parser.STATE_FINDSTART
+                    else:
+                      self.acceptPacket()
+                continue # end STATE_GETCRC
