7a8075ff2857880dc44bed4de452322462fff7fe
Chad Bohannan <chad.bohannan@gmail.com>
2018-03-20 19:14:22 -0600

    Rewriting C# implementation. CRC not working right, yet.

---
+++ examples/ELPForge/ELPForge/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELPForge
+{
+    class Program
+    {
+        class PacketHandler : ELP.IPacketHandler
+        {
+            public void OnPacket(ELP.Packet packet)
+            {
+                Console.Out.WriteLine(String.Format("Packet recieved; {0} data bytes", packet.Data.Length));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ELP.Parser parser = new ELP.Parser(new PacketHandler());
+            using (FileStream input = File.OpenRead("packetstream.input"))
+            {
+                byte[] byteArray = new byte[1];
+                for (int i = 0; i < input.Length; i++)
+                {
+                    int value = input.ReadByte();
+                    byteArray[0] = (byte)value;
+                    parser.readBytes(byteArray);
+                }
+                
+            }
+            Console.Out.WriteLine("press Enter to quit");
+            Console.In.ReadLine();
+        }
+    }
+}
