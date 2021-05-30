7d0312ba891db032243e856d6d4889d39e4d2fe3
Chad Bohannan <chad.bohannan@gmail.com>
2021-05-30 15:52:30 -0700

    TcpChannel connects and receives a packet without blocking.

--- java/aln/src/main/java/org/biglittleidea/aln/TcpChannel.java
+++ java/aln/src/main/java/org/biglittleidea/aln/TcpChannel.java
@@ -3,19 +3,20 @@ package org.biglittleidea.aln;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
-
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.TimeUnit;
 
 public class TcpChannel implements IChannel {
     AsynchronousSocketChannel socket;
 
     public TcpChannel(String host, int port) {
-        try{
+        try {
             InetSocketAddress hostAddress = new InetSocketAddress(host, port);
             socket = AsynchronousSocketChannel.open();
             socket.connect(hostAddress).get(); // block until connection is made
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
-        }        
+        }
     }
 
     public void send(Packet p) {
@@ -23,30 +24,41 @@ public class TcpChannel implements IChannel {
             socket.write(ByteBuffer.wrap(p.toFrameBuffer())).get();
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
-        }        
-    }
-    
-    public void receive(IPacketHandler ph, IChannelCloseHandler ch){
-        Parser parser = new Parser(ph);
-        try{
-            boolean receiving = true;
-            ByteBuffer buffer = ByteBuffer.allocate(4096);
-            while (true) {
-                buffer.flip();
-                int num = socket.read(buffer).get();
-                if (num < 1) { // number of bytes read
-                    ph.onPacket(new Packet());
-                    break;
-                };
-                parser.readBytes(buffer.array(), num);
-            }
-        } catch(Exception e) {
-            System.out.println(e.getLocalizedMessage());
         }
-        ch.onChannelClosed(this);
     }
 
-    public void close(){
+    public void receive(IPacketHandler packetHandler, IChannelCloseHandler closeHandler) {
+        Parser parser = new Parser(packetHandler);
+        if (socket != null && socket.isOpen()) {
+            int buffSize = 4096;
+            ByteBuffer[] buffer = new ByteBuffer[]{ByteBuffer.allocate(buffSize)};
+            try {                
+                int offset = 0;
+                long timeout = 500;
+                CompletionHandler completionHandler = new CompletionHandler<Long,Integer>(){
+                    public void completed(Long result, Integer a){
+                        long num = result.longValue();
+                        if (num < 0) { // number of bytes read
+                            closeHandler.onChannelClosed(TcpChannel.this);
+                        } else if (num > 0) {
+                            parser.readBytes(buffer[0].array(), (int)num);
+                            buffer[0].flip();
+                            socket.read(buffer, 0, 1, timeout, TimeUnit.SECONDS, 42, this);
+                        }
+                    }
+                };
+                socket.read(buffer, 0, 1, timeout, TimeUnit.MILLISECONDS, 42, completionHandler);
+            } catch (Exception e) {
+                System.out.println(e.getLocalizedMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void close() {
+        if (socket == null || !socket.isOpen()) {
+            return;
+        }
         try {
             socket.close();
         } catch (Exception e) {
--- java/aln/src/main/java/org/biglittleidea/aln_client/ClientExample.java
+++ java/aln/src/main/java/org/biglittleidea/aln_client/ClientExample.java
@@ -5,35 +5,28 @@ import org.biglittleidea.aln.IChannelCloseHandler;
 import org.biglittleidea.aln.IPacketHandler;
 import org.biglittleidea.aln.Packet;
 import org.biglittleidea.aln.TcpChannel;
-/**
- * Hello world!
- */
-public final class ClientExample {
-    private ClientExample() {
-    }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+public final class ClientExample {
+
     public static void main(String[] args) {
-        try{
+        try {
             TcpChannel ch = new TcpChannel("localhost", 8000);
             ch.send(new Packet());
-            ch.receive(new IPacketHandler(){
-                public void onPacket(Packet p){
-                    System.out.println("packet received");
+            ch.receive(new IPacketHandler() {
+                public void onPacket(Packet p) {
+                    System.out.println("packet received:");
+                    System.out.println(p.toString());
                 }
-            }, new IChannelCloseHandler(){
-                public void onChannelClosed(IChannel ch){
+            }, new IChannelCloseHandler() {
+                public void onChannelClosed(IChannel ch) {
                     System.out.println("channel closed");
-                }                
+                }
             });
-            Thread.sleep(1000);
+            Thread.sleep(10000);
             ch.close();
-            Thread.sleep(1000);
-            
-        } catch(Exception e) {
+            Thread.sleep(100);
+
+        } catch (Exception e) {
             System.out.print(e.getMessage());
         }
     }
